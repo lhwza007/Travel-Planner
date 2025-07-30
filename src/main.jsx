ff1d032094d017b1786a2c19f8b40f771bfcc506
fahaph <F_Phonwen@hotmail.com>
2025-07-30 17:22:53 +0700

    เพิ่ม bootstrap js

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -10,5 +11,5 @@ createRoot(document.getElementById('root')).render(
   </StrictMode>,
   
 
-  
+
 )
