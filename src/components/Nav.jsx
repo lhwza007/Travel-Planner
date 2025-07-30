7d199ede58f0fca7b2a765eeb91ebbd297144c39
fahaph <F_Phonwen@hotmail.com>
2025-07-30 17:28:35 +0700

    #

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,144 +3,64 @@ import logo from "../assets/logo.svg";
 
 function Nav() {
   return (
-    // <nav
-    //   className="navbar navbar-expand-lg"
-    //   style={{ backgroundColor: "#495A3A", padding: "1.5rem 1.5rem" }}
-    // >
-    //   <div className="container-md">
-    //     <a className="navbar-brand" href="/">
-    //     <img src={logo}  style={{ width: "100px", height: "auto" }} />
-    //     </a>
-
-    //     {/* ปุ่มย่อยุบ */}
-    //     <button
-    //       className="navbar-toggler"
-    //       type="button"
-    //       data-bs-toggle="collapse"
-    //       data-bs-target="#navbarNav"
-    //       aria-controls="navbarNav"
-    //       aria-expanded="false"
-    //       aria-label="Toggle navigation"
-    //     >
-    //       <span className="navbar-toggler-icon"></span>
-    //     </button>
-
-    //     <div className="collapse navbar-collapse" id="navbarNav">
-    //       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-    //         <li className="nav-item">
-    //           <NavLink
-    //             to="/"
-    //             className={({ isActive }) =>
-    //               `nav-link ${isActive ? "active" : ""} text-white`
-    //             }
-    //             style={{ fontWeight: "bold" }}
-    //           >
-    //             Home
-    //           </NavLink>
-    //         </li>
-    //         <li className="nav-item">
-    //           <NavLink
-    //             to="/plan"
-    //             className={({ isActive }) =>
-    //               `nav-link ${isActive ? "active" : ""} text-white`
-    //             }
-    //             style={{ fontWeight: "bold" }}
-    //           >
-    //             Plan
-    //           </NavLink>
-    //         </li>
-    //         <li className="nav-item">
-    //           <NavLink
-    //             to="/favorite"
-    //             className={({ isActive }) =>
-    //               `nav-link ${isActive ? "active" : ""} text-white`
-    //             }
-    //             style={{ fontWeight: "bold" }}
-    //           >
-    //             Favorite
-    //           </NavLink>
-    //         </li>
-    //       </ul>
-    //     </div>
-    //   </div>
-    // </nav>
-
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-          Navbar
+    <nav
+      className="navbar navbar-expand-lg"
+      style={{ backgroundColor: "#495A3A", padding: "1.5rem 1.5rem" }}
+    >
+      <div className="container-md">
+        <a className="navbar-brand" href="/">
+        <img src={logo}  style={{ width: "100px", height: "auto" }} />
         </a>
+
+        {/* ปุ่มย่อยุบ */}
         <button
-          class="navbar-toggler"
+          className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="true"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span class="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                Link
-              </a>
-            </li>
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  `nav-link ${isActive ? "active" : ""} text-white`
+                }
+                style={{ fontWeight: "bold" }}
               >
-                Dropdown
-              </a>
-              <ul class="dropdown-menu">
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Action
-                  </a>
-                </li>
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Another action
-                  </a>
-                </li>
-                <li>
-                  <hr class="dropdown-divider" />
-                </li>
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </li>
-              </ul>
+                Home
+              </NavLink>
             </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" aria-disabled="true">
-                Disabled
-              </a>
+            <li className="nav-item">
+              <NavLink
+                to="/plan"
+                className={({ isActive }) =>
+                  `nav-link ${isActive ? "active" : ""} text-white`
+                }
+                style={{ fontWeight: "bold" }}
+              >
+                Plan
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/favorite"
+                className={({ isActive }) =>
+                  `nav-link ${isActive ? "active" : ""} text-white`
+                }
+                style={{ fontWeight: "bold" }}
+              >
+                Favorite
+              </NavLink>
             </li>
           </ul>
-          <form class="d-flex" role="search">
-            <input
-              class="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button class="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
         </div>
       </div>
     </nav>
