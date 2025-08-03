c75da2dec8700f87561aeaa491899c026141acf3
fahaph <F_Phonwen@hotmail.com>
2025-08-03 18:13:55 +0700

    route

---
+++ src/components/ProfileCard.jsx
@@ -0,0 +1,12 @@
+import "../components/ProfileCard.css";
+
+export default function ProfileCard() {
+  return (
+    <>
+      <div className="profileCardContainer">
+        <div className="cover" style={{backgroundImage:""}}>asdasd</div>
+        <div className="info"></div>
+      </div>
+    </>
+  );
+}
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,12 @@
+import { Container } from "react-bootstrap"
+import ProfileCard from "../components/ProfileCard"
+
+export default function Profile() {
+    return (
+        <>
+        <Container>
+            <ProfileCard />
+        </Container>
+        </>
+    )
+}
