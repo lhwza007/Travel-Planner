9d31d6439465f0b7831852b9bff868b0b3bc1f7b
fahaph <F_Phonwen@hotmail.com>
2025-08-10 03:34:31 +0700

    create api path, create condition for register (if user is alrdy exists or not)

--- backend/index.js
+++ backend/index.js
@@ -1,47 +1,13 @@
-const express = require('express');
-const mysql = require('mysql2');
+import express from "express";
+import authRoutes from "./routes/auth.js";
 
 const app = express();
-const port = 3000;
 
+// middlewares (มันคือไรวะ)
 app.use(express.json());
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "northtrek"
-})
+app.use("/api/auth", authRoutes);
 
-connection.connect((err) => {
-    if (err) {
-        console.error("MySQL Connection is Error ", err);
-        return;
-    }
-    console.log("Connected to MySQL")
-})
-
-
-
-// API endpoint
-app.post('/api/insert', (req, res) => {
-    const { account_name, account_password } = req.body;
-
-    const query = "INSERT INTO accounts(account_name, account_password) VALUES(?, ?)";
-    
-    connection.query(query, [account_name, account_password], (err, result) => {
-        if (err) {
-            console.error("Error inserting data ", err);
-            res.status(500).json({ error: "internal server error"})
-        }
-
-        res.json({
-            msg: "data inserted successfully",
-            insertId: result.insertId
-        })
-    });
-})
-
-app.listen(port, () => {
-    console.log("Server is running on port: ", port);
-})
+app.listen(8800, () => {
+  console.log("API is working...");
+});
