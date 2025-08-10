1cf9c2f973ad4b4c69764186bac4195c9b6d6dbd
Markk <lhwza007x2@gmail.com>
2025-08-10 16:58:38 +0700

    #

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,21 @@
 import express from "express";
 import authRoutes from "./routes/auth.js";
+import cors from 'cors'
 
 const app = express();
 
 // middlewares (มันคือไรวะ)
 app.use(express.json());
+
+app.use((req,res,next)=>{
+  res.header("Access-Control-Allow-Origin","*")
+  res.header("Access-Control-Allow-Header","Origin,X-requested-With,Content-Type,Accept")
+  next()
+  
+})
+
+
+
 
 app.use("/api/auth", authRoutes);
 
