01a23a4070fffb8a6099cb6f123abd2a308b20be
fahaph <F_Phonwen@hotmail.com>
2025-09-02 21:39:28 +0700

    update db

--- database/northtrek.sql
+++ database/northtrek.sql
@@ -31,7 +31,7 @@ CREATE TABLE `activities` (
   `activity_start` time DEFAULT NULL,
   `activity_end` time DEFAULT NULL,
   PRIMARY KEY (`activity_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,6 +40,7 @@ CREATE TABLE `activities` (
 
 LOCK TABLES `activities` WRITE;
 /*!40000 ALTER TABLE `activities` DISABLE KEYS */;
+INSERT INTO `activities` VALUES (1,385141,'asd','19:15:00','19:15:00'),(2,385141,'asd','19:16:00','19:16:00'),(3,385141,'asd','19:16:00','19:16:00'),(4,925456,'asd','19:15:00','19:15:00'),(5,925456,'asd','19:16:00','19:16:00'),(6,925456,'asd','19:16:00','19:16:00'),(7,138467,'asd','19:15:00','19:15:00'),(8,138467,'asd','19:16:00','19:16:00'),(9,138467,'asd','19:16:00','19:16:00'),(10,187050,'กินเบียร์','18:00:00','21:00:00'),(11,187050,'กินเหล้า','21:00:00','22:00:00'),(12,187050,'หม่าล่า','22:00:00','22:55:00'),(13,109308,'8;p','13:00:00','13:00:00'),(14,109308,'su','13:00:00','14:00:00'),(15,944169,'',NULL,NULL),(16,267251,'',NULL,NULL),(17,655680,'asd',NULL,NULL),(18,653192,'แกะถุง (แกง)','20:00:00','21:00:00'),(19,653192,'เสียบ (ชาร์จ ทรศ จากแบตสำรอง)','21:00:00','22:00:00'),(20,653192,'แตก (หัว)','22:00:00','22:00:00'),(21,664673,'ฟไก',NULL,'13:00:00'),(22,664673,'ฟหก','13:00:00','13:00:00'),(23,296122,'ๆไำ',NULL,NULL),(24,849197,'ๆไำ',NULL,NULL);
 /*!40000 ALTER TABLE `activities` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -203,13 +204,15 @@ DROP TABLE IF EXISTS `plans`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `plans` (
   `plan_id` int NOT NULL AUTO_INCREMENT,
-  `user_id` int DEFAULT NULL,
+  `park_id` int NOT NULL,
+  `user_id` int NOT NULL,
   `plan_name` varchar(255) DEFAULT NULL,
   `plan_start` date DEFAULT NULL,
   `plan_end` date DEFAULT NULL,
-  `plan_timeStamp` timestamp NULL DEFAULT NULL,
+  `plan_timeStamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `plan_isPrivate` tinyint(1) NOT NULL,
   PRIMARY KEY (`plan_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=944170 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -218,6 +221,7 @@ CREATE TABLE `plans` (
 
 LOCK TABLES `plans` WRITE;
 /*!40000 ALTER TABLE `plans` DISABLE KEYS */;
+INSERT INTO `plans` VALUES (109308,1,5,'gmujp;','2025-09-02','2025-09-02','2025-09-02 13:49:53',0),(187050,1,5,'เที่ยวผับ','2025-09-02','2025-09-02','2025-09-02 13:43:14',0),(296122,1,5,'ๆไำ','2025-09-02','2025-09-03','2025-09-02 21:27:52',1),(653192,1,5,'เดินป่ากลางคืนกับสาวๆ','2025-09-02','2025-09-02','2025-09-02 19:52:17',0),(664673,2,5,'ฟหก','2025-09-02','2025-09-03','2025-09-02 21:01:07',0),(849197,1,5,'ๆไำ','2025-09-02','2025-09-02','2025-09-02 21:28:09',0);
 /*!40000 ALTER TABLE `plans` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -243,7 +247,7 @@ CREATE TABLE `users` (
   `user_pfp` text,
   `user_timeStamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -252,7 +256,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'test','$2b$10$7LHJGjnc5d6x7AmXtQp9DO8qOp.cXu1AdnukDUvo0nwhBrsbTWUdq',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(2,'test1','$2b$10$mSrcNqQof8izffUk0jhucuu9KyroHjmRB.ZDf7aqHKtCxENIzJTUW',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(3,'test2','$2b$10$fesBsMiRTT9fFimNeW.uBun0lEw7IE92u6srCwgZcpUiCbnz0Er/O',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO `users` VALUES (5,'fahaph','$2b$10$DK4973aHGlLk6.25LqWlVuK8Kz7WwR62aBqurujjK39i1WLMTGja2','Aphidech','Phonwen','Beginner','Male',10000,21,'F_Phonwen@hotmail.com',NULL,NULL,'2025-09-02 06:40:42');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -265,4 +269,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-08-10 18:55:42
+-- Dump completed on 2025-09-02 21:38:24
