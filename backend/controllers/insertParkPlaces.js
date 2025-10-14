e24e135bc25dcfc9bf0faf1953938990c19fb4e8
fahaph <F_Phonwen@hotmail.com>
2025-10-14 15:54:05 +0700

    fixed authenticate status

--- backend/controllers/insertParkPlaces.js
+++ backend/controllers/insertParkPlaces.js
@@ -6,17 +6,18 @@ export const InsertParkPlaces = async (req, res) => {
   const parkplace_lng = req.body.parkplace_lng;
   const parkplace_name = req.body.parkplace_name;
   const parkplace_description = req.body.parkplace_description;
+  const parkplaces_center = JSON.stringify(req.body.parkplaces_center);
 
   try {
     
     const q =
-      "INSERT INTO parkplaces(park_id, parkplace_lat, parkplace_lng, parkplace_name, parkplace_description) VALUES(?, ?, ?, ?, ?)";
+      "INSERT INTO parkplaces(park_id, parkplace_lat, parkplace_lng, parkplace_name, parkplace_description, parkplaces_center) VALUES(?, ?, ?, ?, ?, ?)";
 
     await db
       .promise()
-      .query(q, [park_id, parkplace_lat, parkplace_lng, parkplace_name, parkplace_description]);
+      .query(q, [park_id, parkplace_lat, parkplace_lng, parkplace_name, parkplace_description, parkplaces_center]);
 
-    return res.status(200).json("ParkPlaces has been created.");
+    return res.status(200).json(`ParkPlaces has been created.: ${parkplace_name}`); 
   } catch (error) {
     return res.status(500).json({ error: "Insertion error", details: error.message });
   }
--- frontend/context/checkAuth.jsx
+++ frontend/context/checkAuth.jsx
@@ -1,14 +1,23 @@
-import axios from 'axios';
+import axios from "axios";
 
 export async function checkAuth() {
-  try {
-    const response = await axios.get('http://localhost:8800/api/auth/checkAuth', {
-      withCredentials: true
-    });
+  if (localStorage.getItem("user")) {
+    return true;
 
-    return response.data.isAuthenticated === true; // Assuming the response contains an 'id' field if authenticated
-  } catch (error) {
-    console.error("Error checking authentication:", error);
-    return false;
+  } else {
+    try {
+      const response = await axios.get(
+        "http://localhost:8800/api/auth/checkAuth",
+        {
+          withCredentials: true,
+        }
+      );
+      return response.data.isAuthenticated === true; // Assuming the response contains an 'id' field if authenticated
+
+    } catch (error) {
+      console.error("Error checking authentication:", error);
+      return false;
+      
+    }
   }
 }
