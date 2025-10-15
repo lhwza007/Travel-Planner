06091013ecff8cdd7272ed0da5839bfe2d75d0ce
fahaph <F_Phonwen@hotmail.com>
2025-10-15 16:09:19 +0700

    api for querying data related with user id

--- frontend/src/components/PlanPost.jsx
+++ frontend/src/components/PlanPost.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Form from "react-bootstrap/Form";
-import { Button } from 'react-bootstrap';
+import { Button } from "react-bootstrap";
 import personpfp from "../assets/personTest.svg";
 import money from "../assets/money.svg";
 import comment from "../assets/comment.svg";
@@ -26,51 +26,44 @@ export default function PlanPost({ planData }) {
   verify();
   const navigate = useNavigate();
 
-  
   // console.log(user.user_id); // "123"
 
-  
   const [modalShow, setModalShow] = useState(false);
 
   // const handleOpenModal = () => setModalShow(true);
 
   const handleOpenModal = () => {
-  
-    if(isAuthenticated){
+    if (isAuthenticated) {
       return setModalShow(true);
-    }else{
+    } else {
       return navigate("/login");
     }
-  }
-     
+  };
+
   const handleCloseModal = () => setModalShow(false);
-  
-
-
 
   useEffect(() => {
     getFavoriteStatus();
-     
   }, []);
 
   // ดึงสถานะ isFavorite ของแผนการเดินทางนั้นๆ
   const getFavoriteStatus = () => {
-    if (!user || isAuthenticated === false ) return;
+    if (!user || isAuthenticated === false) return;
     axios
       .get("http://localhost:8800/api/favorite/getFavoriteStatus", {
         params: {
           user_id: user.user_id,
-          plan_id: planData.plan_id
-        }
+          plan_id: planData.plan_id,
+        },
       })
       .then((res) => setIsFavorite(res.data))
       .catch((err) => console.error(err));
-  }
+  };
 
   const toggleFavorite = (e) => {
     e.preventDefault();
 
-    if (!user || isAuthenticated === false ) {
+    if (!user || isAuthenticated === false) {
       navigate("/login");
       return;
     }
@@ -80,107 +73,124 @@ export default function PlanPost({ planData }) {
       axios
         .post("http://localhost:8800/api/favorite/addFavorite", {
           user_id: user.user_id,
-          plan_id: planData.plan_id})
-        .then((res) => //console.log("Success", res.data.success),
-          setIsFavorite(!isFavorite)
+          plan_id: planData.plan_id,
+        })
+        .then(
+          (
+            res //console.log("Success", res.data.success),
+          ) => setIsFavorite(!isFavorite)
         )
         .catch((err) => console.error(err));
-    } else { // ถ้า isFavorite status = true ให้ลบ favorite
+    } else {
+      // ถ้า isFavorite status = true ให้ลบ favorite
       axios
         .post("http://localhost:8800/api/favorite/removeFavorite", {
           user_id: user.user_id,
-          plan_id: planData.plan_id})
-        .then((res) => //console.log("Removed", res.data),
-          setIsFavorite(!isFavorite)
+          plan_id: planData.plan_id,
+        })
+        .then(
+          (
+            res //console.log("Removed", res.data),
+          ) => setIsFavorite(!isFavorite)
         )
         .catch((err) => console.error(err));
     }
-
-    
-
-  }
+  };
 
   const [shareData, setShareData] = useState({
-          sender_id: user?.user_id || null,
-          share_plan_id: planData.plan_id,
-          share_plan_name: planData.plan_name,
-          share_park_name: planData.park_name
-      });
- 
-  
-  
-  
+    sender_id: user?.user_id || null,
+    share_plan_id: planData.plan_id,
+    share_plan_name: planData.plan_name,
+    share_park_name: planData.park_name,
+  });
+
+  const handleProfileClick = (user_id) => {
+    navigate(`/profile?user_id=${user_id}`);
+  };
+
   return (
     <>
-        <div className="container mb-4 planCard">
-          <div className="planCardHeader">
-            <div className="profile">
-              <img src={personpfp} alt="pfp" style={{ width: "50px" }} />
-              <p className="user_name">{planData.user_name}</p>
-            </div>
-          </div>
-
-          <div style={{ fontWeight:"bold", marginBottom:"10px" }}>{planData.park_name}</div>
-          <div className="plan_name">{planData.plan_name}</div>
-
-          <div className="planBudget">
-            <img
-              src={money}
-              alt="budget"
-              style={{ width: "20px", marginRight: "10px" }}
-            />
-            test
-          </div>
-
-          <div className="activities">
-            <ul>
-              {planData.activities.map((activity) => (
-                <li key={activity.activity_id}>
-                  <div className="activityDetails">
-                    <div>{activity.activity_name}</div>
-                    <div>
-                      {activity.activity_start} - {activity.activity_end}
-                    </div>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <hr />
-
-          <div className="actionBar">
-            <div className="favoriteGroup" onClick={toggleFavorite}>
-              <FaStar 
-                style={{color:isFavorite? "orange": "grey", width:"25px", marginBottom:"1px"}}
-              />
-              Favorite
-            </div>
-            <div className="group">
-              <img src={comment} alt="comment" />
-
-              {/* ปุ่มเปิด Modal */}
-              
-                 <img src={share} alt="share" onClick={handleOpenModal} style={{cursor:"pointer"}}/> 
-                 
-               
-              
-            </div>
+      <div className="container mb-4 planCard">
+        <div className="planCardHeader">
+          <div className="profile">
+            <img src={personpfp} alt="pfp" style={{ width: "50px" }} />
+            <p 
+              className="user_name"
+              onClick={() => handleProfileClick(planData.user_id)}
+            >
+              {planData.user_firstName} {planData.user_lastName}
+            </p>
           </div>
         </div>
 
+        <div style={{ fontWeight: "bold", marginBottom: "10px" }}>
+          {planData.park_name}
+        </div>
+        <div className="plan_name">{planData.plan_name}</div>
 
+        <div className="planBudget">
+          <img
+            src={money}
+            alt="budget"
+            style={{ width: "20px", marginRight: "10px" }}
+          />
+          test
+        </div>
+
+        <div className="activities">
+          <ul>
+            {planData.activities.map((activity) => (
+              <li key={activity.activity_id}>
+                <div className="activityDetails">
+                  <div>
+                    {activity.activity_name} ({activity.parkplace_name})
+                  </div>
+                  <div>
+                    {activity.activity_start} - {activity.activity_end}
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <hr />
+
+        <div className="actionBar">
+          <div className="favoriteGroup" onClick={toggleFavorite}>
+            <FaStar
+              style={{
+                color: isFavorite ? "orange" : "grey",
+                width: "25px",
+                marginBottom: "1px",
+              }}
+            />
+            Favorite
+          </div>
+          <div className="group">
+            <img src={comment} alt="comment" />
+
+            {/* ปุ่มเปิด Modal */}
+
+            <img
+              src={share}
+              alt="share"
+              onClick={handleOpenModal}
+              style={{ cursor: "pointer" }}
+            />
+          </div>
+        </div>
+      </div>
 
       {/* เรียกใช้ ShareModal */}
-      {
-        isAuthenticated? <ShareModal
-        show={modalShow}          //กำหนดสถานะการแสดงผล
-        onHide={() => setModalShow(false)} //ฟังก์ชันที่จะทำงานเมื่อต้องการปิด Modal
-        shareData={shareData} // ข้อมูลที่ส่งให้ Modal
-      /> : <div></div>
-      }
-      
-
+      {isAuthenticated ? (
+        <ShareModal
+          show={modalShow} //กำหนดสถานะการแสดงผล
+          onHide={() => setModalShow(false)} //ฟังก์ชันที่จะทำงานเมื่อต้องการปิด Modal
+          shareData={shareData} // ข้อมูลที่ส่งให้ Modal
+        />
+      ) : (
+        <div></div>
+      )}
     </>
   );
 }
-
