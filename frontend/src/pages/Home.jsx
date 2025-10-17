029517c65c468eee65d40c39fe9a9e3671be9f71
Markk <lhwza007x2@gmail.com>
2025-10-17 16:46:28 +0700

    search system -home page -favorite page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,23 +7,65 @@ import MyNav from "../components/Nav.jsx";
 export default function Home() {
 
   const [plans, setPlans] = useState([]);
+//--------------------------------------------------------------Search
+  const [searchTerm, setSearchTerm] = useState("");
+  // ฟังก์ชันกรองข้อมูล
+  const filterPlans = (plansData, term) => {
+    if (!term.trim()) return plansData; // ถ้าว่าง = แสดงทั้งหมด
+    
+    return plansData.filter(plan => 
+      plan.plan_name?.toLowerCase().includes(term.toLowerCase()) ||
+      plan.park_name?.toLowerCase().includes(term.toLowerCase())
+
+    );
+  };
+
+  // ฟังก์ชันรับค่าจาก Search Component
+  const handleSearchInput = (value) => {
+    setSearchTerm(value);
+  };
+  // แสดงข้อความเมื่อไม่มีผลลัพธ์
+  const noResults = plans.length === 0 && searchTerm.trim();
+  console.log(noResults)
+//--------------------------------------------------------------
+
 
   useEffect(() => {
     axios
       .get("http://localhost:8800/api/getData/plans")
-      .then((res) => setPlans(res.data))
+      .then((res) => {
+        const allPlans = res.data;
+        const filteredPlans = filterPlans(allPlans, searchTerm); //ก่อนที่จะแสดงplan ทั้งหมด เรียกใช้fกรองก่อน
+        setPlans(filteredPlans);
+        
+      })
       .catch((err) => console.error(err));
-  }, []);
+  }, [searchTerm]);
 
+  
+console.log(plans)
   return (
     <>
       <MyNav />
-      <Search/>
-      {
-        plans.map((plan) => (
-          <PlanPost key={plan.plan_id} planData={plan} />
-        ))
+      <Search onSearchChange={handleSearchInput}/>
+
+      {noResults?
+        (
+          <div className="container my-5 text-center">
+          <h4>ไม่พบข้อมูล</h4>
+          </div>
+        ):(
+            <>
+              {
+                plans.map((plan) => (
+                  <PlanPost key={plan.plan_id} planData={plan} />
+                ))
+              }
+            </>
+          )
+
       }
+      
     </>
   );
 }
